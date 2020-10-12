ce2967371c9b9850306ca14f0b220e028d46d819
lubianat <tiago.lubiana.alves@usp.br>
2020-10-12 17:51:06 -0300

    test: Add test for loading text

--- misc/salvador/salvador_extractor/tests/test_salvador_extractor.py
+++ misc/salvador/salvador_extractor/tests/test_salvador_extractor.py
@@ -6,5 +6,11 @@ import pytest
 from salvador_extractor import salvador_extractor
 
 def test_content():
-    assert 4 == 4
+
+    deal = salvador_extractor.gazetteDeal("test")
+
+    expected = "test"
+    result = deal.filetext
+
+    assert result == expected
 
