313af56dd70d448aca9177ebf0d23f76203c84c9
lubianat <tiago.lubiana.alves@usp.br>
2020-10-12 17:51:45 -0300

    code: Start building gazetteDeal function

--- misc/salvador/salvador_extractor/salvador_extractor/salvador_extractor.py
+++ misc/salvador/salvador_extractor/salvador_extractor/salvador_extractor.py
@@ -1,1 +1,17 @@
 """Main module."""
+
+
+class gazetteDeal():
+    """
+    A class to describe a deal in a municipal gazette.
+
+    Attributes:
+       filetext: A string containing the text of the deal
+       process: A string containing the process number
+    """
+
+    def __init__(self, filetext):
+        self.filetext = filetext
+
+    
+
