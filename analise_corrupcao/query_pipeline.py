0c2699849032c5a875bdd95ba3716d2325c6c02c
GabrielTrettel <gabrielmtrettel@gmail.com>
2020-10-19 19:40:44 -0300

    code: checking whether a company that donated to a politician was approved to purchase without bidding

--- analise_corrupcao/query_pipeline.py
+++ analise_corrupcao/query_pipeline.py
@@ -1,7 +1,7 @@
-import sqlite3, sys, os, toml, logging
+import sqlite3, sys, os, toml, logging, csv, tqdm
 from sqlite3 import Error
 
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "ERROR"))
 LOGGER = logging.getLogger(__name__)
 
 
@@ -29,7 +29,7 @@ def query_on_database(conn, query):
     return cursor.fetchall()
 
 
-def query_cpf_cnpj_from_socios(conn:sqlite3.Connection, cpf_cnpj:str, fields:list=[], limit:int=-1):
+def get_business_part_of_company(conn:sqlite3.Connection, cpf_cnpj:str, fields:list=[], limit:int=-1):
     """ Create a generator that iterates through all business partners with a
         cpf/cnpj value from `conn` database.
     :conn:     Connection object from sqlite3
@@ -50,8 +50,7 @@ def query_cpf_cnpj_from_socios(conn:sqlite3.Connection, cpf_cnpj:str, fields:lis
 
 
     query = f"""SELECT {fields} FROM socio s
-                WHERE s.cnpj LIKE '{cpf_cnpj}'
-                  OR  s.cnpj_cpf_do_socio LIKE '{cpf_cnpj}' {limit};"""
+                WHERE s.cnpj LIKE '{cpf_cnpj}' {limit};"""
 
     LOGGER.info(f"Running query:\n{query}")
 
@@ -75,6 +74,27 @@ def query_cpf_cnpj_from_election(conn:sqlite3.Connection, cpf_cnpj:str, fields:l
         yield row
 
 
+def verify_match(elections, business_part, buys):
+    company_ids_who_donated = []
+    for buy in tqdm.tqdm(list(buys), ncols=50):
+        company_id = buy['company_id']
+        company_id_clean = company_id.strip(".-/")
+
+        tqdm.tqdm.write(company_id)
+        business_parners = get_business_part_of_company(business_part, company_id_clean, ["cnpj_cpf_do_socio"])
+        for business_parner in business_parners:
+            # If a company owns another company
+            if len(business_parner) > 11:
+                continue
+
+            fields = ['cpf_cnpj_doador', 'setor_economico_doador', 'data', 'tipo_recurso', 'valor', 'descricao']
+            donations = query_cpf_cnpj_from_election(elections, business_parner,fields, 1)
+            if len(list(donations)) > 0:
+                company_ids_who_donated.append(company_id)
+
+    return company_ids_who_donated
+
+
 def main():
     config_file = sys.argv[1]
     configs = toml.load(config_file)
@@ -86,20 +106,10 @@ def main():
     election_db_conn = create_sqlite_connection(elections_db_file)
 
 
-    #  cpf = "04160131000197"
-    cpf = "58560432272"
-    with business_part_db_conn as conn:
-        fields = ["cnpj", "nome_socio", "cnpj_cpf_do_socio"]
-        for business_part in query_cpf_cnpj_from_socios(conn, cpf, fields,  5):
-            print(business_part)
+    buys_fd = open(configs['databases']['compras_csv'])
+    buys_csv = csv.DictReader(buys_fd, delimiter=',', quotechar='"')
 
-
-    print('\n\n')
-
-    with election_db_conn as conn:
-        fields = ['cpf_cnpj_doador', 'setor_economico_doador', 'data', 'tipo_recurso', 'valor', 'descricao']
-        for donation in query_cpf_cnpj_from_election(conn, cpf, fields):
-            print(donation)
+    print(verify_match(election_db_conn, business_part_db_conn, buys_csv))
 
 
 if __name__ == "__main__":
