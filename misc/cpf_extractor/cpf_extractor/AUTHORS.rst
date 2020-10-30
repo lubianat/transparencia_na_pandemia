12e8cb510ddd7cd7dd0d0fa2e1bcb3245b4cb91a
lubianat <tiago.lubiana.alves@usp.br>
2020-10-30 15:30:11 -0300

    chore: Set repo for extracting cpfs

---
+++ misc/cpf_extractor/cpf_extractor/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Tiago Lubiana <tiago.lubiana.alves@usp.br>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ misc/cpf_extractor/cpf_extractor/cpf_extractor/cpf_extractor.py
@@ -0,0 +1,16 @@
+"""Main module."""
+import re
+
+def get_cnpjs(text):
+    """
+    Given a string, extract all CNPJs in it
+
+    Arguments
+        text: A string of text which might contain a CNPJ
+
+    Returns
+        cnpjs: A list of all CNPJs in the string
+
+    """
+    cnpjs = []
+    return(cnpjs)
