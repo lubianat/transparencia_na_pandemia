02ce0da6d15165461fbd5d40d9750a657a78ac5c
lubianat <tiago.lubiana.alves@usp.br>
2020-11-06 13:35:00 -0300

    feat: Add validity checker from Python BR

--- misc/cpf_extractor/cnpj_extractor/cnpj_extractor/cnpj_extractor.py
+++ misc/cpf_extractor/cnpj_extractor/cnpj_extractor/cnpj_extractor.py
@@ -2,6 +2,141 @@
 import re
 import pandas as pd
 
+# Class Cnpj copied from https://wiki.python.org.br/Cnpj 
+# There may be license issues. If there is a problem, we
+# will readily change the code.
+class Cnpj:
+    def __init__( self ):
+        """
+        Class to interact with Cnpj brazilian numbers
+        """
+        pass
+        
+    def validate( self, cnpj ):
+        """ 
+        Method to validate brazilian cnpjs
+        Tests:
+        
+        >>> print Cnpj().validate('61882613000194')
+        True
+        >>> print Cnpj().validate('61882613000195')
+        False
+        >>> print Cnpj().validate('53.612.734/0001-98')
+        True
+        >>> print Cnpj().validate('69.435.154/0001-02')
+        True
+        >>> print Cnpj().validate('69.435.154/0001-01')
+        False
+        """
+        # defining some variables
+        lista_validacao_um = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4 , 3, 2]
+        lista_validacao_dois = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+        
+        # cleaning the cnpj
+        cnpj = cnpj.replace( "-", "" )
+        cnpj = cnpj.replace( ".", "" )
+        cnpj = cnpj.replace( "/", "" )
+    
+        # finding out the digits
+        verificadores = cnpj[-2:]
+        
+        # verifying the lenght of the cnpj
+        if len( cnpj ) != 14:
+            return False
+        
+        # calculating the first digit
+        soma = 0
+        id = 0
+        for numero in cnpj:
+            
+            # to do not raise indexerrors
+            try:
+                lista_validacao_um[id]
+            except:
+                break
+                
+            soma += int( numero ) * int( lista_validacao_um[id] )
+            id += 1
+        
+        soma = soma % 11
+        if soma < 2:
+            digito_um = 0
+        else:
+            digito_um = 11 - soma
+        
+        digito_um = str( digito_um ) # converting to string, for later comparison
+        
+        # calculating the second digit
+        # suming the two lists
+        soma = 0
+        id = 0
+        
+        # suming the two lists
+        for numero in cnpj:
+            
+            # to do not raise indexerrors
+            try:
+                lista_validacao_dois[id]
+            except:
+                break
+            
+            soma += int( numero ) * int( lista_validacao_dois[id] )
+            id += 1
+        
+        # defining the digit
+        soma = soma % 11
+        if soma < 2:
+            digito_dois = 0
+        else:
+            digito_dois = 11 - soma
+        
+        digito_dois = str( digito_dois )
+    
+        # returnig
+        return bool( verificadores == digito_um + digito_dois )
+
+    def format( self, cnpj ):
+        """
+        Method to format cnpj numbers.
+        Tests:
+        
+        >>> print Cnpj().format('53612734000198')
+        53.612.734/0001-98
+        """
+        return "%s.%s.%s/%s-%s" % ( cnpj[0:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:14] )
+
+
+
+
+
+
+
+
+
+
+
+
+
+def check_cnpj(text):
+    """
+    Given a string, extract check if is a properly formatted
+    and valit CNPJ identifier CNPJs
+
+    Arguments
+        text: A string of text which might correspond to a  CNPJ
+
+    Returns
+        is_valid: A Boolean denoting if the cnpj is valid
+
+    """
+
+    pattern = r'\d{2}\.\d{3}\.\d{3}/\d{4}\-\d{2}'
+    match = re.findall(pattern, text)
+    cnpjs = match
+    
+    return(cnpjs)
+
+
 def get_cnpjs(text):
     """
     Given a string, extract all CNPJs in it
--- misc/cpf_extractor/cnpj_extractor/tests/test_cnpj_extractor.py
+++ misc/cpf_extractor/cnpj_extractor/tests/test_cnpj_extractor.py
@@ -21,3 +21,13 @@ def test_that_it_get_cnpjs_for_all_files():
     expected = pd.read_csv("tests/expected_cnpj_df.csv")
     result = cnpj_extractor.get_cnpjs_from_folder("tests")
     assert_frame_equal(expected, result, check_dtype=False)
+
+def test_cnpj_checker():
+    expected = True
+    result = cnpj_extractor.Cnpj().validate('53.612.734/0001-98')
+    assert expected == result
+
+    expected = False
+    result = cnpj_extractor.Cnpj().validate('53.612.734/0001-99')
+    assert expected == result
+
