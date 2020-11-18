be29cdf33f4ae4566449c523c667d9c240cc5b4d
lubianat <tiago.lubiana.alves@usp.br>
2020-11-18 16:12:21 -0300

    code: Format tests for okf repo

--- misc/cpf_extractor/cnpj_extractor/tests/cnpj_extractor_unittest.py
+++ misc/cpf_extractor/cnpj_extractor/tests/cnpj_extractor_unittest.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from cnpj_extractor import cnpj_extractor
+from pandas.testing import assert_frame_equal
 import unittest  
 
 class TestThings(unittest.TestCase):
@@ -14,12 +15,12 @@ class TestThings(unittest.TestCase):
         result = cnpj_extractor.get_cnpjs(self.text)
         self.assertEqual(expected, result)
     
-    def test_that_it_get_cnpjs_for_all_files():
+    def test_that_it_get_cnpjs_for_all_files(self):
         expected = pd.read_csv("tests/expected_cnpj_df.csv")
-        result = cnpj_extractor.get_cnpjs_from_folder("tests")
+        result = cnpj_extractor.get_cnpjs_from_path("tests")
         assert_frame_equal(expected, result, check_dtype=False)
 
-    def test_cnpj_checker():
+    def test_cnpj_checker(self):
         expected = True
         result = cnpj_extractor.check_cnpj('53.612.734/0001-98')
         assert expected == result
